325f5f02e078d916cb707078996e80fa5b14c150
Guilherme Toni <guilhermedelly8@gmail.com>
2023-01-30 12:36:33 -0300

    feat: update logs page

--- src/pages/logs/LogsPage.vue
+++ src/pages/logs/LogsPage.vue
@@ -1,6 +1,23 @@
 <template>
-  <q-page class="">
-    <span class="text-h1">LogsPage</span>
+  <q-page class="logs-page full-height">
+    <div class="row full-width content-center items-center q-mb-xl">
+      <p class="text-h3 col-grow q-mr-md q-mb-none">Logs</p>
+    </div>
+
+    <template v-if="false">
+      [Put the logs content here]
+    </template>
+
+    <template v-else>
+      <div class="row col-12 justify-center items-center logs-no-data">
+        <div class="col-12 text-center">
+          <img src="img/empty-dash.svg" width="450" height="350" alt="">
+          <p class="text-weight-bold">
+            Your don't have logs yet!
+          </p>
+        </div>
+      </div>
+    </template>
   </q-page>
 </template>
 
@@ -11,5 +28,21 @@ export default {
 </script>
 
 <script setup lang="ts">
-// import { Todo, Meta } from 'components/models';
 </script>
+
+<style lang="scss">
+$header-height: 50px;
+$padding-y: 32px;
+$title-height: 50px + 48px; // 48px margin bottom
+
+$used-area: $header-height + $padding-y + ($title-height * 2);
+
+.q-table__card {
+  box-shadow: none;
+  border: 1px solid rgba($secondary, .3);
+}
+
+.logs-no-data {
+  height: calc(100vh - #{$used-area})!important;
+}
+</style>
