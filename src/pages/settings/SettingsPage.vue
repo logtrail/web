f19ff12775dc22f0e74be5e375be5c86d062b9ce
Guilherme Toni <guilhermedelly8@gmail.com>
2023-01-30 12:36:10 -0300

    feat: add settings empty page

--- src/pages/settings/SettingsPage.vue
+++ src/pages/settings/SettingsPage.vue
@@ -1,6 +1,23 @@
 <template>
-  <q-page class="">
-    <span class="text-h1">SettingsPage</span>
+  <q-page class="settings-page full-height">
+    <div class="row full-width content-center items-center q-mb-xl">
+      <p class="text-h3 col-grow q-mr-md q-mb-none">Settings</p>
+    </div>
+
+    <template v-if="false">
+      [Put the settings content here]
+    </template>
+
+    <template v-else>
+      <div class="row col-12 justify-center items-center settings-no-data">
+        <div class="col-12 text-center">
+          <img src="img/empty-settings.svg" width="450" height="350" alt="">
+          <p class="text-weight-bold">
+            Ops! This page is under construction!
+          </p>
+        </div>
+      </div>
+    </template>
   </q-page>
 </template>
 
@@ -11,5 +28,21 @@ export default {
 </script>
 
 <script setup lang="ts">
-// import { Todo, Meta } from 'components/models';
 </script>
+
+<style lang="scss">
+$header-height: 50px;
+$padding-y: 32px;
+$title-height: 50px + 48px; // 48px margin bottom
+
+$used-area: $header-height + $padding-y + ($title-height * 2);
+
+.q-table__card {
+  box-shadow: none;
+  border: 1px solid rgba($secondary, .3);
+}
+
+.settings-no-data {
+  height: calc(100vh - #{$used-area})!important;
+}
+</style>
