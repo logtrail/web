e96e0fa3813aff603dba7f9d7a571061c2f28568
Guilherme Toni <guilhermedelly8@gmail.com>
2023-01-30 11:37:25 -0300

    feat: implement new routes
    OBS: need to break in context files

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -4,7 +4,27 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    children: [{ path: '', component: () => import('pages/logs/LogsPage.vue') }],
+  },
+  {
+    path: '/categories',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/categories/CategoriesPage.vue') }],
+  },
+  {
+    path: '/logtypes',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/logtypes/LogTypesPage.vue') }],
+  },
+  {
+    path: '/notifications',
+    component: () => import('layouts/NotificationsLayout.vue'),
+    children: [{ path: '', component: () => import('pages/notifications/NotificationsPage.vue') }],
+  },
+  {
+    path: '/settings',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/settings/SettingsPage.vue') }],
   },
 
   // Always leave this as last one,
