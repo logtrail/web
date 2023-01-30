f2f96a9284665b368c73dffb708b833e2439d022
Guilherme Toni <guilhermedelly8@gmail.com>
2023-01-30 11:39:35 -0300

    feat: add columns type on pages

---
+++ src/pages/notifications/columns.type.ts
@@ -0,0 +1,18 @@
+export type Columns = {
+  field: string | ((row: any) => any);
+  label: string;
+  name: string,
+
+  align?: 'left' | 'right' | 'center';
+  classes?: string | ((row: any) => string);
+  format?: (val: any, row: any) => any;
+  headerStyle?: string;
+  headerClasses?: string;
+  required?: boolean;
+  sortable?: boolean;
+  sort?: (a: any, b: any, rowA: any, rowB: any) => number;
+  sortOrder?: 'ad' | 'da';
+  style?: string | ((row: any) => string);
+};
+
+export default Columns;
