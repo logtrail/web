f312e78fed41d4d023f1414a75d5749709852eec
Guilherme Toni <guilhermedelly8@gmail.com>
2023-02-08 16:44:30 -0300

    feat: update app vue to use color themes

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,20 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted } from 'vue';
 
+onMounted(() => {
+  const themeStartName = 'theme__';
+  const defaultThemeName = 'yellow';
+
+  const theme = `${themeStartName}${process.env.THEME_COLOR || defaultThemeName}`;
+
+  let bodyClasses = (document.body.classList.value).split(' ');
+
+  bodyClasses = bodyClasses
+    .filter((currentClass: string) => !currentClass.startsWith(themeStartName));
+
+  bodyClasses.push(theme);
+  document.body.classList.add(...bodyClasses);
+});
 </script>
