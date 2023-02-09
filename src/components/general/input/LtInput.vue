822f5578560c82903972a5cc965195e76dda5832
Thiago Anselmo <thiagoo.anselmoo@gmail.com>
2023-02-09 18:34:31 -0300

    :hammer: Fix console errors

--- src/components/general/input/LtInput.vue
+++ src/components/general/input/LtInput.vue
@@ -1,61 +1,61 @@
   <template>
   <q-input v-bind="componentDefaultProps">
     <template
-      v-if="slots.default"
+      v-if="slots?.default"
       v-slot:default>
       <slot></slot>
     </template>
 
     <template
-      v-if="slots.prepend"
+      v-if="slots?.prepend"
       v-slot:prepend>
       <slot name="prepend"></slot>
     </template>
 
     <template
-      v-if="slots.append"
+      v-if="slots?.append"
       v-slot:append>
       <slot name="append"></slot>
     </template>
 
     <template
-      v-if="slots.before"
+      v-if="slots?.before"
       v-slot:before>
       <slot name="before"></slot>
     </template>
 
     <template
-      v-if="slots.after"
+      v-if="slots?.after"
       v-slot:after>
       <slot name="after"></slot>
     </template>
 
     <template
-      v-if="slots.label"
+      v-if="slots?.label"
       v-slot:label>
       <slot name="label"></slot>
     </template>
 
     <template
-      v-if="slots.error"
+      v-if="slots?.error"
       v-slot:error>
       <slot name="error"></slot>
     </template>
 
     <template
-      v-if="slots.hint"
+      v-if="slots?.hint"
       v-slot:hint>
       <slot name="hint"></slot>
     </template>
 
     <template
-      v-if="slots.counter"
+      v-if="slots?.counter"
       v-slot:counter>
       <slot name="counter"></slot>
     </template>
 
     <template
-      v-if="slots.loading"
+      v-if="slots?.loading"
       v-slot:loading>
       <slot name="loading"></slot>
     </template>
